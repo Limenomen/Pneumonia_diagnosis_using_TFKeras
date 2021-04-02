d38a647ab2042cde5dd34e6ddaf4c6163678f59d
Your Name <rsagadatov@gmail.com>
2021-04-02 16:36:46 +0500

    restart!

--- train_network.py
+++ train_network.py
@@ -14,55 +14,6 @@ from tensorflow.keras.utils import to_categorical
 
 
 #'Путь, где находятся изображения для датасета'
-train_path = '../chest_xray/train'
-test_path = '../chest_xray/test'
+train_path = '../../chest_xray/train'
+test_path = '../../chest_xray/test'
 
-train_gen = tf.keras.preprocessing.image_dataset_from_directory(
-  train_path,
-  validation_split=0.2,
-  color_mode="grayscale",
-  batch_size=32,
-  image_size=(180, 180),
-  shuffle=True,
-  seed=123,
-  subset="training",
-)
-
-test_gen = tf.keras.preprocessing.image_dataset_from_directory(
-  test_path,
-  validation_split=0.2,
-  color_mode="grayscale",
-  batch_size=32,
-  image_size=(180, 180),
-  shuffle=True,
-  seed=123,
-  subset="validation",
-)
-
-
-model = tf.keras.Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(32, 3, activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(2)
-])
-
-
-model.compile(
-  optimizer='adam',
-  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['accuracy'])
-
-history = model.fit(
-  train_gen,
-  validation_data=test_gen,
-  epochs=3
-)
-
-model.save('my_model')
