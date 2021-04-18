2b694a3b8760a86b7e6a95e0356412ef29015c22
Your Name <rsagadatov@gmail.com>
2021-04-18 19:04:57 +0500

    test

--- train_network.py
+++ train_network.py
@@ -3,18 +3,18 @@ import numpy as np
 import cv2
 import PIL
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import RMSprop, Adam
-from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
+plt.plot([1, 2, 3, 4, 5],[1, 2, 3, 4, 5])
+plt.show()
 
 
 #'Путь, где находятся изображения для датасета'
 train_path = '../../chest_xray/train'
 test_path = '../../chest_xray/test'
+val_path = '../../chest_xray/val'
 
-fdgdf
+
+def load_img():
+    
+
