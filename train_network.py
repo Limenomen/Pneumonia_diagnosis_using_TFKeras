ff8aa27aaaad6c2664349f31026b9e212b1e6922
Your Name <rsagadatov@gmail.com>
2021-04-18 20:07:29 +0500

    someshit

--- train_network.py
+++ train_network.py
@@ -1,20 +1,28 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import os
 import PIL
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4, 5],[1, 2, 3, 4, 5])
-plt.show()
+
 
 
 #'Путь, где находятся изображения для датасета'
 train_path = '../../chest_xray/train'
 test_path = '../../chest_xray/test'
 val_path = '../../chest_xray/val'
+labels = ['Pneumonia', 'Normal']
+
+def get_images_data(dir):
+    data = []
+    for label in labels:
+        path = os.path.join(dir, label)
+        for img in os.listdir(path):
+            image = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+            resized_img = 
 
 
-def load_img():
     
 
