6b5b9c2210f27f6f18f3a2297d71be63b1f6c935
Your Name <rsagadatov@gmail.com>
2021-05-23 14:04:26 +0500

    23.05

--- train_network.py
+++ train_network.py
@@ -13,7 +13,7 @@ from keras import backend as K
 import seaborn as sns
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
-epochs_count = 10
+epochs_count = 16
 
 
 def get_images_data(dir):
@@ -48,19 +48,19 @@ def graphics():
     val_loss = history.history['val_loss']
     fig.set_size_inches(20, 10)
 
-    ax[0].plot(epochs, train_acc, 'go-', label='Training Accuracy')
-    ax[0].plot(epochs, val_acc, 'ro-', label='Validation Accuracy')
-    ax[0].set_title('Training & Validation Accuracy')
+    ax[0].plot(epochs, train_acc, 'go-', label='точность на тренировочной выборке')
+    ax[0].plot(epochs, val_acc, 'ro-', label='точность на валидационной выборке')
+    ax[0].set_title('Точность на тренеровочной и валидационной выборке')
     ax[0].legend()
-    ax[0].set_xlabel("Epochs")
-    ax[0].set_ylabel("Accuracy")
+    ax[0].set_xlabel("эпоха")
+    ax[0].set_ylabel("точность")
 
-    ax[1].plot(epochs, train_loss, 'g-o', label='Training Loss')
-    ax[1].plot(epochs, val_loss, 'r-o', label='Validation Loss')
-    ax[1].set_title('Testing Accuracy & Loss')
+    ax[1].plot(epochs, train_loss, 'g-o', label='потери на тренировочной выборке')
+    ax[1].plot(epochs, val_loss, 'r-o', label='потери на валидационной выборке')
+    ax[1].set_title('точность и потери на тестовой выборке')
     ax[1].legend()
-    ax[1].set_xlabel("Epochs")
-    ax[1].set_ylabel("Training & Validation Loss")
+    ax[1].set_xlabel("эпоха")
+    ax[1].set_ylabel("потери")
     plt.show()
     l = []
     for i in train:
@@ -105,49 +105,48 @@ datagen.fit(x_train)
 
 model = Sequential()
 
-model.add(Conv2D(32 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu' , input_shape = (150,150,1)))
-model.add(BatchNormalization())
-model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
+model.add(Conv2D(32, (3, 3), strides=1, padding='same',
+                 activation='relu', input_shape=(150, 150, 1)))
+model.add(MaxPool2D((2, 2), strides=2, padding='same'))
 
-model.add(Conv2D(64 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu'))
-model.add(Dropout(0.1))
-model.add(BatchNormalization())
-model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
-
-model.add(Conv2D(64 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu'))
-model.add(BatchNormalization())
-model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
-
-model.add(Conv2D(128 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu'))
+model.add(Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))
 model.add(Dropout(0.2))
 model.add(BatchNormalization())
-model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
+model.add(MaxPool2D((2, 2), strides=2, padding='same'))
 
-model.add(Conv2D(256 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu'))
+model.add(Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))
 model.add(Dropout(0.2))
 model.add(BatchNormalization())
-model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
+model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+
+model.add(Conv2D(128, (3, 3), strides=1, padding='same', activation='relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding='same'))
+
+model.add(Conv2D(256, (3, 3), strides=1, padding='same', activation='relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding='same'))
 
 model.add(Flatten())
-model.add(Dense(units = 128 , activation = 'relu'))
+model.add(Dense(units=128, activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(units = 1 , activation = 'sigmoid'))
-model.compile(optimizer = "rmsprop" , loss = 'binary_crossentropy' , metrics = ['accuracy'])
+model.add(Dense(units=1, activation='sigmoid'))
+model.compile(optimizer="adam", loss='binary_crossentropy',
+              metrics=['accuracy'])
 model.summary()
 
-"""
 earlystopping = EarlyStopping(monitor='val_loss',
                               mode='min',
-                              patience=3,
+                              patience=5,
                               verbose=1)
-"""
-learning_rate_reduction = ReduceLROnPlateau(
-    monitor='val_accuracy', patience=2, verbose=1, factor=0.3, min_lr=0.00001)
 
+learning_rate_reduction = ReduceLROnPlateau(
+    monitor='val_accuracy', patience=3, verbose=1, factor=0.3, min_lr=0.00001)
 
 history = model.fit(datagen.flow(x_train, y_train, batch_size=32), epochs=epochs_count,
                     validation_data=datagen.flow(x_val, y_val), callbacks=[learning_rate_reduction])
-
 
 print("Loss of the model is - ", model.evaluate(x_test, y_test)[0])
 print("Accuracy of the model is - ",
